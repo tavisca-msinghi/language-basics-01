78d94f3b4a791bc8c9b784326cf42c8873ec516c
tavisca-msinghi <msinghi@tavisca.in>
2019-06-24 15:29:10 +0530

    complete first language assignment in program file

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -23,7 +23,75 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int FindDigit(string equation)
         {
             // Add your code here.
-            throw new NotImplementedException();
+            
+            char []equal = {'='};
+            string []arr=equation.Split(equal);
+            string right=arr[1];
+            char []multi = {'*'};
+            int a=0,b=0,c=0;
+            string []left=arr[0].Split(multi);
+            string l1=left[0];
+            string l2=left[1];
+            for(int i=0;i<l1.Length;i++){
+                if(l1[i]=='?') a=1;
+            }
+            for(int i=0;i<l2.Length;i++){
+                if(l2[i]=='?') b=1;
+            }
+            for(int i=0;i<right.Length;i++){
+                if(right[i]=='?') c=1;
+            }
+            //Console.WriteLine("a= {0}",l1 );
+            if(a==1){
+                int num1=Int32.Parse(l2);
+                int num2=Int32.Parse(right);
+                int ans=num2/num1;
+                
+                if(num2%num1==0){
+                    int j=l1.Length-1;
+                    while(ans!=0){
+                        int rem=ans%10;
+                        if(l1[j]=='?') return rem;
+                        ans=ans/10;
+                        j--;
+                    }
+                }
+                else return -1;
+            }
+            else{
+                    if(b==1){
+                        int num1=Int32.Parse(l1);
+                        int num2=Int32.Parse(right);
+                        int ans=num2/num1;
+                        
+                        if(num2%num1==0){
+                            int j=l2.Length-1;
+                            while(ans!=0){
+                            int rem=ans%10;
+                            if(l2[j]=='?') return rem;
+                            ans=ans/10;
+                            j--;
+                        }
+                    }
+                    else return -1; 
+                    }
+                    else{
+                        int num1=Int32.Parse(l1);
+                        int num2=Int32.Parse(l2);
+                        int ans=num2*num1;
+                            int j=right.Length-1;
+                            while(ans!=0){
+                            int rem=ans%10;
+                            if(right[j]=='?') return rem;
+                            ans=ans/10;
+                            j--;
+                            }
+                            return -1;
+                    }
+                    return -1;
+            }
+            return -1;
+           // throw new NotImplementedException();
         }
     }
 }
